e44e398fd5fc00620afd0abaed5739db83e24a75
“funhappyit” <fun.happyit@gmail.com>
2024-04-14 20:42:50 +0900

    2024-04-14 1)JPA 쇼핑몰 프로젝트 엔티티 생성

---
+++ src/main/java/jpashop/domain/Movie.java
@@ -0,0 +1,13 @@
+package jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorValue("M")
+public class Movie extends Item{
+
+    private String director;
+    private String actor;
+
+
+}
---
+++ src/main/java/jpashop/domain/OrderStatus.java
@@ -0,0 +1,10 @@
+package jpashop.domain;
+
+/**
+ * Created by holyeye on 2014. 3. 11..
+ */
+public enum OrderStatus {
+
+    ORDER, CANCEL
+
+}
