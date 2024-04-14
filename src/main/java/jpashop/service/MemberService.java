b885d1eadcb1f055a692136b629b2f81fb7e80d9
“funhappyit” <fun.happyit@gmail.com>
2024-04-15 00:11:38 +0900

    2024-04-15 1)회원가입 기능

---
+++ src/main/java/jpashop/service/MemberService.java
@@ -0,0 +1,49 @@
+package jpashop.service;
+
+import jpashop.domain.Member;
+import jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /*
+    회원가입
+    */
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        List<Member> findMembers =
+                memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    /*
+    전체 회원 조회
+    */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+
+
+
+
+}
